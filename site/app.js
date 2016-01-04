fda09dc8ca7a53519b2eab51c2681ce3ee26fd7a
John Coker <john@jcsw.com>
2016-01-03 22:16:21 -0800

    make favicon from mobile app icon

--- site/app.js
+++ site/app.js
@@ -29,7 +29,6 @@ app.engine('hbs', require('exphbs'));
 app.set('view engine', 'hbs');
 
 // other Express configuration
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

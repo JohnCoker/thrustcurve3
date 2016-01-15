f47022f67c861f05a51801280efd24c3dc719e4c
John Coker <john@jcsw.com>
2016-01-14 21:55:45 -0800

    use natural paths and implemented editing for manufacturers

--- routes/manufacturers.js
+++ routes/manufacturers.js
@@ -12,79 +12,134 @@ var defaults = {
   layout: 'info',
 };
 
-router.get('/manufacturers/list.html', function(req, res, next) {
+router.get(['/manufacturers/', '/manufacturers/list.html'], function(req, res, next) {
   req.db.Manufacturer.find({}, undefined, { sort: { name: 1 } }, req.success(function(results) {
     for (var i = 0; i < results.length; i++) {
-      if (results[i].website)
-        results[i].websiteDomain = results[i].website.replace(/^https?:\/+/i, '')
-                                                     .replace(/^www\./, '')
-                                                     .replace(/\/.*$/, '');
+      results[i].motorsLink = '/manufacturers/' + results[i].abbrev + '/motors.html';
+      results[i].editLink = '/manufacturers/' + results[i]._id + '/edit.html';
     }
     res.render('manufacturers/list', locals(defaults, {
       title: 'Motor Manufacturers',
-      manufacturers: results
+      manufacturers: results,
+      newLink: '/manufacturers/new/edit.html'
     }));
   }));
 });
 
-router.get('/manufacturers/motors.html', function(req, res, next) {
-  var id = req.query.id;
-  if (req.db.isId(id)) {
-    req.db.Manufacturer.findOne({ _id: id }, req.success(function(manufacturer) {
-      var query, unavailable = false;
-      if (!manufacturer) {
-        res.redirect(303, '/manufacturers/list.html');
-      } else {
-        query = {
-          $or: [ { _manufacturer: manufacturer._id }, { _relatedMfr: manufacturer._id } ]
-        };
-        if (req.query.unavailable !== '' && req.query.unavailable !== 'true')
-          query.availability = { $in: req.db.schema.MotorAvailableEnum };
-        else
-          unavailable = true;
-        req.db.Motor.find(query, undefined, { sort: { totalImpulse: 1, designation: 1 } }, req.success(function(motors) {
-          res.render('manufacturers/motors', locals(defaults, {
-            title: 'Motors by ' + manufacturer.abbrev,
-            manufacturer: manufacturer,
-            motors: motors,
-            unavailable: unavailable
-          }));
+function mfrQuery(req) {
+  if (req.db.isId(req.query.id))
+    return { _id: req.query.id };
+
+  if (req.db.isId(req.params.id))
+    return { _id: req.params.id };
+
+  var name = req.params.name;
+  if (!name)
+    name = 'none';
+  return { $or: [ { name: name }, { abbrev: name } ] };
+}
+
+router.get('/manufacturers/:name/motors.html', function(req, res, next) {
+  req.db.Manufacturer.findOne(mfrQuery(req), req.success(function(manufacturer) {
+    var motorsQuery, unavailable = false;
+    if (!manufacturer) {
+      res.redirect(303, '/manufacturers/');
+    } else {
+      motorsQuery = {
+        $or: [ { _manufacturer: manufacturer._id }, { _relatedMfr: manufacturer._id } ]
+      };
+      if (req.query.unavailable !== '' && req.query.unavailable !== 'true')
+        motorsQuery.availability = { $in: req.db.schema.MotorAvailableEnum };
+      else
+        unavailable = true;
+      req.db.Motor.find(motorsQuery, undefined, { sort: { totalImpulse: 1, designation: 1 } }, req.success(function(motors) {
+        res.render('manufacturers/motors', locals(defaults, {
+          title: manufacturer.abbrev + ' Motors',
+          manufacturer: manufacturer,
+          motors: motors,
+          unavailable: unavailable,
+          unavailableLink: '/manufacturers/' + manufacturer.abbrev + '/motors.html?unavailable'
         }));
-      }
-    }));
-  } else {
-    res.redirect(303, '/manufacturers/list.html');
-  }
+      }));
+    }
+  }));
 });
 
-router.get('/manufacturers/edit.html', function(req, res, next) {
-  var id = req.query.id;
-  if (req.db.isId(id)) {
-    req.db.Manufacturer.findOne({ _id: id }, req.success(function(result) {
-      if (result) {
-        res.render('manufacturers/edit', locals(defaults, {
-          title: 'Edit ' + result.abbrev,
-          manufacturer: result,
-          isEdit: true
-        }));
-      } else {
-        res.render('manufacturers/edit', locals(defaults, {
-          title: 'New Manufacturer',
-          manufacturer: { active: true },
-          isNew: true
-        }));
-      }
-    }));
-  } else {
-    res.render('manufacturers/edit', locals(defaults, {
-      title: 'New Manufacturer',
-      manufacturer: { active: true },
-      isNew: true
-    }));
-  }
+router.get('/manufacturers/:id/edit.html', function(req, res, next) {
+  req.db.Manufacturer.findOne(mfrQuery(req), req.success(function(result) {
+    if (result) {
+      res.render('manufacturers/edit', locals(defaults, {
+        title: 'Edit ' + result.abbrev,
+        manufacturer: result,
+        isEdit: true,
+        submitLink: '/manufacturers/' + result._id + '/edit.html',
+        isCreated: req.query.result == 'created',
+        isSaved: req.query.result == 'saved',
+        isUnchanged: req.query.result == 'unchanged'
+      }));
+    } else {
+      res.render('manufacturers/edit', locals(defaults, {
+        title: 'New Manufacturer',
+        manufacturer: { active: true },
+        isNew: true,
+        submitLink: '/manufacturers/new/edit.html'
+      }));
+    }
+  }));
 });
 
-router.post('/manufacturers/edit.html', function(req, res, next) {
+router.post('/manufacturers/:id/edit.html', function(req, res, next) {
+  req.db.Manufacturer.findOne(mfrQuery(req), req.success(function(manufacturer) {
+    var isNew = false, isChanged = false,
+        aliases;
+
+    if (manufacturer == null) {
+      manufacturer = {
+        aliases: [],
+        active: true
+      };
+      isNew = true;
+    }
+
+    ['name', 'abbrev', 'website'].forEach(function(p) {
+      if (req.body.hasOwnProperty(p) && req.body[p] != manufacturer[p]) {
+        manufacturer[p] = req.body[p]
+        isChanged = true;
+      }
+    });
+
+    if (req.body.aliases) {
+      aliases = req.body.aliases.split(/ *,[ ,]*/);
+      if (aliases.join() != manufacturer.aliases.join()) {
+        manufacturer.aliases = aliases;
+        isChanged = true;
+      }
+    }
+
+    if (req.body.active) {
+      if (!manufacturer.active) {
+        manufacturer.active = true;
+        isChanged = true;
+      }
+    } else {
+      if (manufacturer.active) {
+        manufacturer.active = false;
+        isChanged = true;
+      }
+    }
+
+    if (isNew) {
+      req.db.Manufacturer.create(new req.db.Manufacturer(manufacturer), req.success(function(updated) {
+        res.redirect(303, '/manufacturers/' + updated._id + '/edit.html?result=created');
+      }));
+    } else if (isChanged) {
+      manufacturer.save(req.success(function() {
+        res.redirect(303, '/manufacturers/' + manufacturer._id + '/edit.html?result=saved');
+      }));
+    } else {
+      res.redirect(303, '/manufacturers/' + manufacturer._id + '/edit.html?result=unchanged');
+    }
+  }));
 });
 
 // old site compatibility
@@ -94,12 +149,12 @@ router.get(['/manufacturers.shtml'], function(req, res, next) {
     // old-style MySQL row ID; go to motor list
     req.db.Manufacturer.findOne({ migratedId: id }, req.success(function(result) {
       if (result)
-        res.redirect(301, '/manufacturers/motors.html?id=' + result._id);
+        res.redirect(301, '/manufacturers/' + result._abbrev + '/motors.html');
       else
-        res.redirect(301, '/manufacturers/list.html');
+        res.redirect(303, '/manufacturers/');
     }));
   } else {
-    res.redirect(301, '/manufacturers/list.html');
+    res.redirect(301, '/manufacturers/');
   }
 });
 

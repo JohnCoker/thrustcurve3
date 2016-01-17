8132278faee78b3436e1ea9c778832c1b7187e9f
John Coker <john@jcsw.com>
2016-01-16 18:17:04 -0800

    added set of histograms on motor detail page

--- routes/motors.js
+++ routes/motors.js
@@ -14,31 +14,66 @@ var defaults = {
 
 var searchLink = '/motors/search.html';
 
-router.get('/motors/:mfr/:desig/', function(req, res, next) {
+var classCounts = {};
+
+function getClassCount(req, cls, cb) {
+  if (classCounts.hasOwnProperty(cls)) {
+    cb(classCounts[cls]);
+    return;
+  }
+
+  req.db.Motor.count({ impulseClass: cls }, req.success(function(count) {
+    classCounts[cls] = count;
+    cb(count);
+  }));
+}
+
+function getMotor(req, res, parents, redirect, cb) {
   req.db.Manufacturer.findOne({ abbrev: req.params.mfr }, req.success(function(manufacturer) {
-    if (manufacturer == null)
-      res.redirect(303, searchLink);
+    if (manufacturer == null) {
+      if (redirect)
+        res.redirect(303, searchLink);
+      else
+        res.status(404).send('unknown manufacturer abbreviation ' + req.params.mfr);
+    }
     else {
-      req.db.Motor.findOne({ designation: req.params.desig }).populate('_relatedMfr _certOrg').exec(req.success(function(motor) {
-        if (motor == null)
-          res.redirect(303, searchLink);
-        else {
-          req.db.SimFile.find({ _motor: motor._id }, undefined, { sort: { updatedAt: -1 } }).populate('_contributor').exec(req.success(function(simfiles) {
-            req.db.MotorNote.find({ _motor: motor._id }, undefined, { sort: { updatedAt: -1 } }).populate('_contributor').exec(req.success(function(notes) {
-              res.render('motors/details', locals(defaults, {
-                title: manufacturer.abbrev + ' ' + motor.designation,
-                manufacturer: manufacturer,
-                motor: motor,
-                simfiles: simfiles,
-                notes: notes,
-                editLink: req.helpers.motorLink(manufacturer, motor) + 'edit.html'
-              }));
-            }));
-          }));
+      var q = req.db.Motor.findOne({ designation: req.params.desig });
+      if (parents)
+        q = q.populate('_relatedMfr _certOrg');
+      q.exec(req.success(function(motor) {
+        if (motor == null) {
+          if (redirect)
+            res.redirect(303, searchLink);
+          else
+            res.status(404).send('unknown motor designation ' + req.params.desig);
+        } else {
+          // count all the motors in this class
+          getClassCount(req, motor.impulseClass, function(count) {
+            cb(motor, manufacturer, count);
+          });
         }
       }));
     }
   }));
+}
+
+router.get('/motors/:mfr/:desig/', function(req, res, next) {
+  getMotor(req, res, true, true, function(motor, manufacturer, classCount) {
+    req.db.SimFile.find({ _motor: motor._id }, undefined, { sort: { updatedAt: -1 } }).populate('_contributor').exec(req.success(function(simfiles) {
+      req.db.MotorNote.find({ _motor: motor._id }, undefined, { sort: { updatedAt: -1 } }).populate('_contributor').exec(req.success(function(notes) {
+        res.render('motors/details', locals(defaults, {
+          title: manufacturer.abbrev + ' ' + motor.designation,
+          manufacturer: manufacturer,
+          motor: motor,
+          simfiles: simfiles,
+          notes: notes,
+          classCount: classCount,
+          isCompare: classCount >= 5,
+          editLink: req.helpers.motorLink(manufacturer, motor) + 'edit.html'
+        }));
+      }));
+    }));
+  });
 });
 router.get('/motorsearch.jsp', function(req, res, next) {
   var id = req.query.id;
@@ -53,6 +88,128 @@ router.get('/motorsearch.jsp', function(req, res, next) {
   } else {
     res.redirect(301, searchLink);
   }
+});
+
+var classHistograms = {};
+
+function getHistogram(req, cls, stat, cb) {
+  var key = cls + ':' + stat;
+  if (classHistograms.hasOwnProperty(key)) {
+    cb(classHistograms[key]);
+    return;
+  }
+
+  var q = { impulseClass: cls };
+  q[stat] = { $gt: 0 };
+  req.db.Motor.find(q, '_id ' + stat, req.success(function(all) {
+    var min = all[0][stat], max = all[0][stat],
+        range, histogram, v, i, j;
+
+    if (all.length < 3) {
+      classHistograms[key] = null;
+      cb(null);
+      return;
+    }
+
+    for (i = 1; i < all.length; i++) {
+      v = all[i][stat];
+      if (v < min)
+        min = v;
+      if (v > max)
+        max = v;
+    }
+    range = max - min;
+    range += range / 1000;
+
+    // generate histogram
+    histogram = {
+      cls: cls,
+      stat: stat,
+      n: all.length >= 50 ? 10 : 5,
+      buckets: [],
+      minX: min,
+      maxX: max,
+      rangeX: range,
+      count: 0,
+      maxY: 0
+    };
+    for (i = 0; i < histogram.n; i++)
+      histogram.buckets[i] = 0;
+    for (i = 0; i < all.length; i++) {
+      v = all[i][stat];
+      j = Math.floor(((v - min) / range) * histogram.n);
+      histogram.buckets[j]++;
+      if (histogram.buckets[j] > histogram.maxY)
+        histogram.maxY = histogram.buckets[j];
+      histogram.count++;
+    }
+
+    classHistograms[key] = histogram;
+    cb(histogram);
+  }));
+}
+
+router.get('/motors/:mfr/:desig/compare.svg', function(req, res, next) {
+  var stat;
+
+  // determine the motor statistic
+  if (!req.query.stat) {
+    res.status(404).send('motor statistic not specified');
+    return;
+  }
+  if (req.query.stat == 'impulse')
+    stat = 'totalImpulse';
+  else if (req.query.stat == 'thrust')
+    stat = 'avgThrust';
+  else if (req.query.stat == 'burn')
+    stat = 'burnTime';
+  else
+    stat = req.query.stat;
+  if (!req.db.Motor.schema.paths.hasOwnProperty(stat)) {
+    res.status(404).send('unknown motor statistic ' + stat);
+    return;
+  }
+
+  // load this motor
+  getMotor(req, res, false, false, function(primary) {
+    // get the histogram for this impulse class and stat
+    getHistogram(req, primary.impulseClass, stat, function(histogram) {
+      var graph, w, x, x1, y, i;
+
+      if (histogram == null) {
+        res.status(404).send('too little data for ' + stat);
+        return;
+      }
+
+      graph = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none">\n';
+
+      // draw baseline
+      graph += '  <line x1="0" y1="99.5" x2="100" y2="99.5" stroke="#ccc" stroke-width="1" />\n';
+
+      // graph histogram
+      x = 0;
+      w = (100 - (histogram.n - 1)) / histogram.n;
+      for (i = 0; i < histogram.n; i++) {
+        x1 = x + w;
+
+        if (histogram.buckets[i] > 0) {
+          y = 2 + 98 * (histogram.buckets[i] / histogram.maxY);
+          graph += '  <rect x="' + x + '" y="' + (100 - y) + '" width="' + w + '" height="' + y + '" fill="#ccc" />\n';
+        }
+
+        x = x1 + 1;
+      }
+
+      // graph primary motor's value
+      if (primary[stat] > 0) {
+        x = (100 * (primary[stat] - histogram.minX) / histogram.rangeX) - 0.5;
+        graph += '  <line x1="' + x + '" y1="1" x2="' + x + '" y2="99" stroke="#9e1a20" stroke-width="1" />\n';
+      }
+
+      graph += '</svg>';
+      res.status(200).type('svg').send(graph);
+    });
+  });
 });
 
 router.get(searchLink, function(req, res, next) {

e9f831e84f2c4456260a539e50911460b10ee088
Daniel_M_Williams <equipoise@gmail.com>
2016-12-22 16:51:48 -0500

    [upgrade] Converted database spec from 'jasmine-node' to 'jasmine-core'

--- database/schema/spec/schema-spec.js
+++ database/schema/spec/schema-spec.js
@@ -1,318 +1,269 @@
-var mongoose = require('mongoose'),
-    schema = require('..');
+var mongoose = require('mongoose');
+var schema = require('..');
 
-describe('schema', function() {
-  describe('setup', function() {
-    it('connect', function() {
-      mongoose.Promise = global.Promise;
+mongoose.Promise = require('bluebird')
+var mockgoose = require('mockgoose')
+
+describe('schemas', function() {
+  var startTime;
+
+  beforeAll( function(done) {
+    mockgoose(mongoose).then(function() {
       mongoose.connect('mongodb://localhost/test', function(err) {
-        if (err)
-          console.error(err);
-        else 
-          mongoose.connection.db.dropDatabase();
+        startTime = new Date();
+        if (err){ console.error(err); }
+        done(err);
       });
-      waits(100);
+    });
+  });
+
+  afterAll( function( done) {
+    mongoose.disconnect();
+    done();
+  });
+
+  afterEach( function(done){
+    mockgoose.reset(function() {
+      done();
     });
   });
 
   describe('manufacturer', function() {
-    var model = schema.ManufacturerModel(mongoose),
-        startTime = new Date();
+    var ManufacturerModel = null;
 
-    it('save', function() {
-      runs(function() {
-        var inst = model({
+    beforeAll( function(done){
+      ManufacturerModel = schema.ManufacturerModel(mongoose);
+      done();
+    });
+
+    beforeEach( function(done){
+      new ManufacturerModel({
           name: 'Rockets R Us',
           abbrev: 'RRU',
           aliases: ['RocketsRUs', 'RRUS'],
           website: 'http://rocketsrus.com/'
-        });
-        inst.save(function(err) {
-          if (err)
-            console.error(err);
-        });
+      }).save(function(err) {
+        expect(err).toBeNull();
+        done();
       });
-      waits(100);
     });
 
-    it('find abbrev', function() {
-      var found;
-      waits(150);
-      runs(function() {
-        model.findOne({ abbrev: 'RRU' }, function(err, results) {
-          if (err)
-            console.error(err);
-          found = results;
-        });
+    it('manufacturers may be saved to the database.', function( done) {
+      ManufacturerModel.count().exec().then( function( cnt ){
+          expect(cnt).toBeGreaterThan( 0);
+          done();
       });
-      waits(150);
-      runs(function() {
+    });
+
+    it('can find manufacturer by abbrev', function( done) {
+      ManufacturerModel.findOne({ abbrev: 'RRU' }).exec()
+      .then( function( found ){
         var now = new Date();
 
         expect(found).toBeDefined();
-        expect(found).not.toBe(null);
-        if (found != null) {
-          expect(found._id).toBeDefined();
+        expect(found).not.toBeNull();
+        expect(found._id).toBeDefined();
 
-          expect(found.createdAt).toBeDefined();
-          expect(found.createdAt).toBeGreaterThan(startTime);
-          expect(found.createdAt).toBeLessThan(now);
+        expect(found.createdAt).toBeDefined();
+        expect(found.createdAt).toBeGreaterThan(startTime);
+        expect(found.createdAt).toBeLessThan(now);
 
-          expect(found.updatedAt).toBeDefined();
-          expect(found.updatedAt).toBeGreaterThan(startTime);
-          expect(found.updatedAt).toBeLessThan(now);
+        expect(found.updatedAt).toBeDefined();
+        expect(found.updatedAt).toBeGreaterThan(startTime);
+        expect(found.updatedAt).toBeLessThan(now);
 
-          expect(found.name).toBe('Rockets R Us');
-          expect(found.abbrev).toBe('RRU');
-          expect(found.aliases.join()).toBe('RocketsRUs,RRUS');
-          expect(found.website).toBe('http://rocketsrus.com/');
-          expect(found.active).toBe(true);
-        }
+        expect(found.name).toBe('Rockets R Us');
+        expect(found.abbrev).toBe('RRU');
+        expect(found.aliases.join()).toBe('RocketsRUs,RRUS');
+        expect(found.website).toBe('http://rocketsrus.com/');
+        expect(found.active).toBe(true);
+
+        done();
       });
+
     });
-    it('find active', function() {
-      var found;
-      waits(100);
-      runs(function() {
-        model.find({ active: true }, function(err, results) {
-          if (err)
-            console.error(err);
-          else
-            found = results;
-        });
-      });
-      waits(100);
-      runs(function() {
-        expect(found).toBeDefined();
-        expect(found.length).toBeGreaterThan(0);
+
+    it('can find active manufacturers', function( done) {
+      ManufacturerModel.find({ active: true }).count().exec()
+      .then( function(count) {
+        expect( count).toBeDefined();
+        expect( count).toBeGreaterThan(0);
+        done();
       });
     });
 
-    it('update', function() {
+    it('update', function( done) {
       var queryTime, createTime, found, updated;
 
-      waits(100);
       queryTime = new Date();
-      runs(function() {
-        model.findOne({ abbrev: 'RRU' }, function(err, results) {
-          if (err)
-            console.error(err);
-          else
-            found = results;
-        });
-      });
-      waits(100);
-      runs(function() {
-        expect(found).toBeDefined();
-        expect(found).not.toBe(null);
-        if (found != null) {
-          expect(found.createdAt).toBeGreaterThan(startTime);
-          expect(found.createdAt).toBeLessThan(queryTime);
-          expect(found.updatedAt).toBeGreaterThan(startTime);
-          expect(found.updatedAt).toBeLessThan(queryTime);
-          createTime = found.createdAt;
-          found.aliases.push('RUS');
-          found.save(function(err, results) {
-            if (err)
-              console.error(err);
-          });
-        }
-      });
-      waits(100);
-      runs(function() {
-        model.findOne({ abbrev: 'RRU' }, function(err, results) {
-          if (err)
-            console.error(err);
-          else
-            updated = results;
-        });
-      });
-      waits(100);
-      runs(function() {
-        var now = new Date();
+      ManufacturerModel.findOne({ abbrev: 'RRU' }).exec()
+      .then( function( priorFound) {
+        expect(priorFound).toBeDefined();
 
-        expect(updated).toBeDefined();
-        expect(updated).not.toBe(null);
-        if (updated != null) {
-          expect(updated._id.toString()).toBe(found._id.toString());
-          expect(updated.createdAt.toISOString()).toBe(createTime.toISOString());
-          expect(updated.updatedAt).toBeGreaterThan(queryTime);
-          expect(updated.updatedAt).toBeLessThan(now);
-          expect(updated.aliases.indexOf('RUS')).toBeGreaterThan(-1);
-        }
+        priorFound.aliases.push('RUS');
+        priorFound.save( function(){
+
+          ManufacturerModel.findOne({ abbrev: 'RRU' }).exec()
+          .then( function( updated){
+            var now = new Date();
+
+            expect(updated).toBeDefined();
+            expect(updated).not.toBeNull();
+            expect(updated._id.toString()).toBe( priorFound._id.toString());
+            expect(updated.createdAt.toISOString()).toBe( priorFound.createdAt.toISOString());
+            expect(updated.updatedAt).toBeGreaterThan(queryTime);
+            expect(updated.updatedAt).toBeLessThan(now);
+            expect(updated.aliases.indexOf('RUS')).toBeGreaterThan(-1);
+
+            done();
+          });
+        });
       });
     });
   });
 
+  // certorg (aka Certification Orginization )
   describe('certorg', function() {
-    var model = schema.CertOrgModel(mongoose);
-    it('save', function() {
-      runs(function() {
-        var inst = model({
-          name: 'Good Rocketry Association',
-          abbrev: 'GRA',
-          aliases: ['GoodRocketry', 'Good'],
-          website: 'http://goodrocketry.org/'
-        });
-        inst.save(function(err) {
-          if (err)
-            console.error(err);
-        });
-      });
-      waits(100);
+    var CertOrgModel = null;
+
+    beforeAll( function(done){
+      CertOrgModel = schema.CertOrgModel(mongoose);
+      done();
     });
 
-    it('find abbrev', function() {
-      var found;
-      waits(100);
-      runs(function() {
-        model.findOne({ abbrev: 'GRA' }, function(err, results) {
-          if (err)
-            console.error(err);
-          else
-            found = results;
-        });
-      });
-      waits(100);
-      runs(function() {
-        expect(found).toBeDefined();
-        expect(found).not.toBe(null);
-        if (found != null) {
-          expect(found._id).toBeDefined();
-          expect(found.createdAt).toBeDefined();
-          expect(found.updatedAt).toBeDefined();
-          expect(found.name).toBe('Good Rocketry Association');
-          expect(found.abbrev).toBe('GRA');
-          expect(found.aliases.join()).toBe('GoodRocketry,Good');
-          expect(found.website).toBe('http://goodrocketry.org/');
-        }
+    beforeEach( function(done){
+      new CertOrgModel({
+        name: 'Good Rocketry Association',
+        abbrev: 'GRA',
+        aliases: ['GoodRocketry', 'Good'],
+        website: 'http://goodrocketry.org/'
+      }).save(function(err) {
+        expect(err).toBeNull();
+        done();
       });
     });
-    it('find active', function() {
-      var found;
-      waits(100);
-      runs(function() {
-        model.find({ active: true }, function(err, results) {
-          if (err)
-            console.error(err);
-          else
-            found = results;
-        });
+
+    it('certorgs may be saved to the database', function( done) {
+      CertOrgModel.count().exec()
+      .then( function( cnt ){
+        expect(cnt).toBeGreaterThan( 0);
+        done();
       });
-      waits(100);
-      runs(function() {
+    });
+
+    it('find abbrev', function( done){
+      CertOrgModel.findOne({ abbrev: 'GRA' }).exec()
+      .then( function( found) {
+
         expect(found).toBeDefined();
-        expect(found.length).toBeGreaterThan(0);
+        expect(found).not.toBeNull();
+
+        expect(found._id).toBeDefined();
+        expect(found.createdAt).toBeDefined();
+        expect(found.updatedAt).toBeDefined();
+        expect(found.name).toBe('Good Rocketry Association');
+        expect(found.abbrev).toBe('GRA');
+        expect(found.aliases.join()).toBe('GoodRocketry,Good');
+        expect(found.website).toBe('http://goodrocketry.org/');
+        done();
+      });
+    });
+
+    it('find active', function( done) {
+      CertOrgModel.findOne({ active: true }).count().exec()
+      .then( function( orgCount ) {
+        expect( orgCount).toBeDefined();
+        expect( orgCount).toBeGreaterThan(0);
+        done();
       });
     });
   });
 
   describe('contributor', function() {
-    var model = schema.ContributorModel(mongoose);
+    var ContributorModel = null;
 
-    it('save', function() {
-      runs(function() {
-        var inst = model({
-          email: 'joe@xample.com',
-	  name: 'Joe Anyone',
-	  organization: 'XYZ Rocket Club',
-          password: 'very secret',
-	  showEmail: true,
-	  website: 'http://example.com/joes-rockets.html',
-        });
-        inst.save(function(err) {
-          if (err)
-            console.error(err);
-        });
-      });
-      waits(100);
+    beforeAll( function(done){
+      ContributorModel = schema.ContributorModel(mongoose);
+      done();
     });
 
-    it('verify saved', function() {
-      var found;
-      waits(100);
-      runs(function() {
-        model.findOne({ email: 'joe@xample.com' }, function(err, results) {
-          if (err)
-            console.error(err);
-          found = results;
-        });
+    beforeEach( function( done){
+      new ContributorModel({
+          email: 'joe@xample.com',
+      	  name: 'Joe Anyone',
+      	  organization: 'XYZ Rocket Club',
+          password: 'very secret',
+      	  showEmail: true,
+      	  website: 'http://example.com/joes-rockets.html',
+      }).save(function(err) {
+        expect(err).toBeNull();
+        done();
       });
-      waits(100);
-      runs(function() {
+    });
+
+    it('certorgs may be saved to the database', function( done) {
+      ContributorModel.count().exec()
+      .then( function( cnt ){
+        expect(cnt).toBeGreaterThan( 0);
+        done();
+      });
+    });
+
+    it('verify saved', function( done ) {
+      ContributorModel.findOne({ email: 'joe@xample.com' }).exec()
+      .then( function( found ){
+
         expect(found).toBeDefined();
         expect(found.email).toBe('joe@xample.com');
-	expect(found.password).toMatch(/^\$2a\$11\$[a-zA-Z0-9+\/.]+=*$/);
-	expect(found.name).toBe('Joe Anyone');
-	expect(found.organization).toBe('XYZ Rocket Club');
+      	expect(found.password).toMatch(/^\$2a\$11\$[a-zA-Z0-9+\/.]+=*$/);
+      	expect(found.name).toBe('Joe Anyone');
+      	expect(found.organization).toBe('XYZ Rocket Club');
         expect(found.showEmail).toBe(true);
-	expect(found.website).toBe('http://example.com/joes-rockets.html');
+	      expect(found.website).toBe('http://example.com/joes-rockets.html');
+
+        done();
       });
     });
-    it('comparePassword good', function() {
-      var found, wasMatch;
-      waits(100);
-      runs(function() {
-        model.findOne({ email: 'joe@xample.com' }, function(err, results) {
-          if (err)
-            console.error(err);
-          found = results;
+
+    it('contributor password match', function( done ) {
+      ContributorModel.findOne({ email: 'joe@xample.com' }).exec()
+      .then( function( found) {
+
+        expect(found).toBeDefined();
+      	found.comparePassword('very secret', function(err, isMatch) {
+      	  if (err){
+      	    console.error(err);
+            fail('passwords do not match');
+      	  }else{
+      	    expect(isMatch).toBe(true);
+            done();
+          }
         });
-      });
-      waits(100);
-      runs(function() {
-	expect(found).toBeDefined();
-	found.comparePassword('very secret', function(err, isMatch) {
-	  if (err)
-	    console.error(err);
-	  else
-	    wasMatch = isMatch;
-	});
-      });
-      waits(300);
-      runs(function() {
-	expect(wasMatch).toBe(true);
       });
     });
-    it('comparePassword good', function() {
-      var found, wasMatch;
-      waits(100);
-      runs(function() {
-        model.findOne({ email: 'joe@xample.com' }, function(err, results) {
-          if (err)
-            console.error(err);
-          found = results;
+
+    it('comparePassword fail', function( done) {
+      ContributorModel.findOne({ email: 'joe@xample.com' }).exec()
+      .then( function( found) {
+        expect(found).toBeDefined();
+
+	      found.comparePassword('vary sacred', function(err, isMatch) {
+      	  if (err){
+      	    console.error(err);
+      	  }else{
+      	    expect(isMatch).toBe(false);
+            done();
+          }
         });
-      });
-      waits(100);
-      runs(function() {
-	expect(found).toBeDefined();
-	found.comparePassword('vary sacred', function(err, isMatch) {
-	  if (err)
-	    console.error(err);
-	  else
-	    wasMatch = isMatch;
-	});
-      });
-      waits(300);
-      runs(function() {
-	expect(wasMatch).toBe(false);
       });
     });
-    it('hasPermission', function() {
-      var found;
-      waits(100);
-      runs(function() {
-        model.findOne({ email: 'joe@xample.com' }, function(err, results) {
-          if (err)
-            console.error(err);
-          found = results;
-        });
-      });
-      waits(300);
-      runs(function() {
-        expect(found.hasPermission('editMotors')).toBe(false);
-        expect(found.hasPermission('noSuchPerm')).toBe(false);
+
+    it('hasPermission', function( done) {
+      ContributorModel.findOne({ email: 'joe@xample.com' }).exec()
+      .then( function( found) {
+        expect( found.hasPermission('editMotors')).toBe(false);
+        expect( found.hasPermission('noSuchPerm')).toBe(false);
+        done();
       });
     });
   });
@@ -336,10 +287,4 @@ describe('schema', function() {
     });
   });
 
-  describe('teardown', function() {
-    it('disconnect', function() {
-      waits(1000);
-      mongoose.disconnect();
-    });
-  });
 });

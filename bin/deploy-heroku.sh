343bfacf09eb8cb6ae55126d08823890b24a9a7a
John Coker <john@jcsw.com>
2017-02-03 16:23:08 -0800

    check for uncommited changes

--- bin/deploy-heroku.sh
+++ bin/deploy-heroku.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -e
 
-git branch -D heroku
-git checkout -b heroku
+git diff-index --quiet HEAD -- || (
+    echo "There are uncomitted changes." >&2
+    git status --short
+    exit 1
+)
+git branch -D heroku || true
+
+git checkout -b heroku --quiet
 cp hosting/heroku/* .
 git add .
-git commit -m 'Heroku config files'
-git push --set-upstream origin heroku --force
-git checkout master
+git commit -m 'Heroku config files' --quiet
+git push --set-upstream origin heroku --force --quiet
+git checkout master --quiet
